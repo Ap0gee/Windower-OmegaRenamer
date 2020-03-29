950ce089dfee6497e8e51da1a64aa73a9523c918
Sjshovan <SJSHOVAN@GMAIL.COM>
2020-03-29 03:00:38 -0400

    initial

---
+++ README.md
@@ -0,0 +1,132 @@
+**Author:** [Sjshovan (LoTekkie)](https://github.com/Ap0gee)  
+**Version:** v0.9.0  
+
+
+# Omega Renamer
+
+> Official Omega private server Windower 4 addon that fixes npc names through automatic updates.
+
+
+### Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Aliases](#aliases)
+- [Usage](#usage)
+- [Commands](#commands)
+- [Support](#support)
+- [Change Log](#change-log)
+- [Known Issues](#known-issues)
+- [TODOs](#todos)
+- [License](#license)
+
+___
+### Prerequisites
+1. [Final Fantasy 11 Online](http://www.playonline.com/ff11us/index.shtml)
+2. [Windower 4](http://windower.net/)
+
+___
+### Installation
+
+This addon is part of the Omega private server ecosystem and comes with the offical Omega server install.   
+If you are using a separate Final Fantasy 11 install you will need to follow the manual install instructions below.
+
+**Manual:**
+1. Navigate to <https://github.com/Ap0gee/Windower-OmegaRenamer>.
+2. Click on `Releases`. 
+3. Click on the `Source code (zip)` link within the latest release to download.
+4. Extract the zipped folder to `Windower4/addons/`.
+5. Rename the folder to remove the version tag (`-v0.9.0`). The folder should be named `OmegaRenamer`.
+
+___
+### Aliases
+The following aliases are available to Omega Renamer commands:    
+
+**omegarenamer:** orenamer | oren   
+**reload:** r  
+**about:** a  
+**help:** h   
+
+___
+### Usage
+
+Manually load the addon by using one of the following commands:
+    
+    //lua load omegarenamer 
+    //lua l omegarenamer
+
+___    
+### Commands 
+
+**help**
+
+Displays the available Omega Renamer commands. Below are the equivalent ways of calling the command:
+
+    //omegarenamer help
+    //orenamer help
+    //oren help
+    
+    //omegarenamer h
+    //orenamer h
+    //oren h
+
+**reload**
+
+Reloads the Omega Renamer addon. Below are the equivalent ways of calling the command:
+    
+    //omegarenamer reload
+    //orenamer reload
+    //oren reload
+    
+    //omegarenamer r
+    //orenamer r
+    //oren r
+    
+**about**
+
+Displays information about the Battle Stations addon. Below are the equivalent ways of calling the command:
+    
+    //omegarenamer about
+    //orenamer about
+    //oren about
+    
+    //omegarenamer a
+    //orenamer a
+    //oren a
+
+___
+### Support
+**Having Issues with this addon?**
+* Please let me know [here](https://github.com/Ap0gee/Windower-OmegaRenamer/issues/new).
+  
+**Have something to say?**
+* Send me some feedback here: <sjshovan@gmail.com>
+
+**Want to stay in the loop with my work?**
+* You can follow me at: <https://twitter.com/Sjshovan>
+
+**Want to toss a coin to your modder?**
+* You can do so here: <https://www.paypal.me/Sjshovan>  
+
+___
+### Change Log
+
+**v0.9.0** - 3/29/2020
+- Initial release
+
+___
+### Known Issues
+
+- **None** 
+___    
+### TODOS
+
+- **TODO:** Consider versioning the map files.
+___
+
+### License
+
+Copyright © 2020, [Sjshovan (LoTekkie)](https://github.com/Ap0gee).
+Released under the [BSD License](LICENSE).
+
+***
---
+++ omegarenamer.lua
@@ -0,0 +1,122 @@
+--[[
+Copyright © 2020, Sjshovan (LoTekkie)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Battle Stations nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Sjshovan (LoTekkie) BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
+
+_addon.name = "Omega Renamer"
+_addon.author = "Sjshovan (LoTekkie) sjshovan@gmail.com"
+_addon.description = 'Official Omega private server addon that fixes npc names through automatic updates.'
+_addon.version = '0.9.0'
+_addon.commands = {'omegarenamer', 'orenamer', 'oren'}
+
+require("luau");
+local config = require("config")
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+local files = require("files")
+
+require('helpers')
+
+local defaults = {}
+local settings = config.load(defaults)
+
+local help = {
+    commands = {
+        buildHelpSeperator('=', 28),
+        buildHelpTitle('Commands'),
+        buildHelpSeperator('=', 28),
+        buildHelpCommandEntry('reload', 'Reload Omega Renamer.'),
+        buildHelpCommandEntry('about', 'Display information about Omega Renamer.'),
+        buildHelpCommandEntry('help', 'Display Omega Renamer commands.'),
+        buildHelpSeperator('=', 28),
+    },
+	about = {
+        buildHelpSeperator('=', 23),
+        buildHelpTitle('About'),
+        buildHelpSeperator('=', 23),
+        buildHelpTypeEntry('Name', _addon.name),
+        buildHelpTypeEntry('Description', _addon.description),
+        buildHelpTypeEntry('Author', _addon.author),
+        buildHelpTypeEntry('Version', _addon.version),
+        buildHelpSeperator('=', 23),
+    },
+}
+
+function displayHelp(table_help)
+    for index, command in pairs(table_help) do
+        displayResponse(command)
+    end
+end
+
+windower.register_event('load', function ()
+	if not files.exists("data/map.lua") then
+		local f = files.new("data/map.lua")
+		files.create(f)
+	end
+	local response = {}
+	http.request{
+		method = "GET",
+		url = "https://omega-renamer.s3.amazonaws.com/omega.lua",
+		sink = ltn12.sink.table(response)
+	}
+	local remoteMap = table.concat(response)
+	files.write("data/map.lua", remoteMap)
+	require("data.map")
+end)
+
+windower.register_event('addon command', function(command, ...)
+	if command then
+        command = command:lower()
+    else 
+        displayHelp(help.commands)
+        return
+    end
+  
+    local command_args = {...}
+   
+	if command == 'reload' or command == 'r' then
+        windower.send_command('lua r omegarenamer')
+        
+    elseif command == 'about' or command == 'a' then
+        displayHelp(help.about)
+
+    elseif command == 'help' or command == 'h' then
+        displayHelp(help.commands)
+             
+    else
+        displayHelp(help.commands)
+    end	
+end)
+
+windower.register_event("prerender", function()
+	local zoneId = windower.ffxi.get_info().zone;
+	local npcs = map[zoneId];
+	if (npcs ~= nil) then
+		for _, data in pairs(npcs) do
+			windower.set_mob_name(data[1], data[2]);
+		end
+	end
+end);
